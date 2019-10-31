9493ca903fa75ca5c2b8c2970aa99ea678e743c7
rihab <rihabelkhadiri@hotmail.com>
2019-10-31 16:00:46 +0100

    .

--- views/books.php
+++ views/books.php
@@ -2,5 +2,17 @@
 <?php ob_start();?>
 <h1>Books list</h1>
 <p>Welcome to the page of books list here you can find the books you admire </p>
+<ul>
+  <?php
+  foreach ($books as $book) {
+    ?>
+    <li><?php echo $book['title']; ?></li>
+  <?php
+  }
+  ?>
+</ul>
+
+<pre><?php var_dump($books); ?></pre>
+
 <?php $content= ob_get_clean(); ?>
 <?php require('public/index.php'); ?>
