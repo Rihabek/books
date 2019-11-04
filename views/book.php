3d44221d6df11d4295393e6b511957a2f0eec377
rihab <rihabelkhadiri@hotmail.com>
2019-11-04 16:57:56 +0100

    victory

--- views/book.php
+++ views/book.php
@@ -1,7 +1,51 @@
 <?php $title='Book List' ?>
 <?php ob_start(); ?>
 
-<?php echo $book['title'];  ?>
+<div class="container display-flex mt-5">
+  <div class="row">
+    <div class="col-4">
+      <div class="image">
+        <img
+          src="public/<?php echo $book['imageLink']; ?>"
+          class="card-img-top"
+          alt="Image du livre <?php echo $book['title']; ?>"/>
+      </div>
 
+  </div>
+    <div class="col-8">
+
+      <h1 style="color:pink;"><?php echo $book['title'];  ?></h1>
+      <table class="table table-striped table-dark mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Author</th>
+          <td scope="col"><?php echo $book["author"]; ?></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Country</th>
+          <td><?php echo $book["country"]; ?></td>
+        </tr>
+        <tr>
+          <th scope="row">Language</th>
+          <td><?php echo $book["language"]; ?></td>
+        </tr>
+        <tr>
+          <th scope="row">Link</th>
+          <td> <a href="<?php echo $book["link"]; ?>" target="_blank">wikipedia</a></td>
+        </tr>
+        <tr>
+          <th scope="row">Pages</th>
+          <td><?php echo $book["pages"]; ?></td>
+        </tr>
+        <tr>
+          <th scope="row">Year</th>
+          <td><?php echo $book["year"] ?></td>
+        </tr>
+
+    </div>
+
+</div>
 <?php $content = ob_get_clean(); ?>
 <?php require('public/index.php'); ?>
--- views/books.php
+++ views/books.php
@@ -7,12 +7,25 @@
   text-overflow: ellipsis;
   white-space: nowrap;
   margin-bottom: 0.8rem;
-  height: 40px;
   display: flex;
   align-items: center;
 }
+
+.info:last-child {
+  margin-bottom: 0;
+}
 .material-icons{
   margin-right: 0.9rem;
+}
+
+.image {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 300px;
+}
+.image img {
+  height: 100%;
 }
 
 </style>
@@ -27,25 +40,25 @@
             <img
               src="public/<?php echo $book['imageLink']; ?>"
               class="card-img-top"
-              alt="Image du livre <?php echo $book['title']; ?>">
+              alt="Image du livre <?php echo $book['title']; ?>"/>
           </div>
           <div class="card-body">
             <h5 class="card-title"><?php echo $book['title']; ?></h5>
             <div class="card-body">
               <div class="infos">
                 <div class="info">
-                  <i class="material-icons">create</i>
+                  <i style="color:#4C0517;"class="material-icons">create</i>
                   <?php echo $book['author']; ?>
                 </div>
                 <div class="info">
-                  <i class="material-icons">date_range</i>
+                  <i style="color:#4C0517;" class="material-icons">date_range</i>
                   <?php echo $book["year"] ?>
                 </div>
               </div>
             </div>
           </div>
-          <div class="card-footer text-muted">
-            <a href="<?php echo $book['link']; ?>" class="btn btn-danger">Informations</a>
+          <div class="card-footer text-muted text-center">
+            <a href="?action=book&id=<?php echo $book['id']; ?>" class="btn btn-danger ">Informations</a>
           </div>
 
         </div>
