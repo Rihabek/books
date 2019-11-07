c0047f039ea936dac7ce0fa42dec69ef0c0efe1f
rihab <rihabelkhadiri@hotmail.com>
2019-11-07 13:51:45 +0100

    commit1

--- admin/index.php
+++ admin/index.php
@@ -20,36 +20,75 @@ require('../utils/db.php');
      <title></title>
    </head>
    <body>
-     <div class="container">
+    <div class="container">
        <form class="./" action="index.html" method="post">
-         <div class="row">
-           <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-6">
              <div class="form-group">
               <label for="title">Book title</label>
               <input name="title" maxlength="30" type="email" class="form-control" id="title" placeholder="Book's title">
               <small id="titlehelp" class="form-text text-muted">Books title between 0 and 255.</small>
              </div>
              <div class="form-group">
-               <label for="description">Description</label>
-               <textarea name="description" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <label for="description">Description</label>
+              <textarea name="description" class="form-control" id="description" rows="3"></textarea>
              </div>
              <div class="form-group">
-                <label for="author">Authors</label>
-                <select name='author_id' class="form-control" id="author_id">
+                <label for="author_id">Authors</label>
+                <select name="author_id" class="form-control" id="author_id">
                   <?php foreach ($a_id as $author ) {?>
-
-                  <option value="<?php echo $author['id']; ?>">
-                    <?php echo $author['name']; ?>
-                  </option>
-                <?php } ?>
-
+                    <option value="<?php echo $author['id']; ?>">
+                      <?php echo $author['name']; ?>
+                    </option>
+                  <?php } ?>
                 </select>
               </div>
+              <div class="form-group">
+               <label for="pages">Pages number</label>
+               <input name="pages"
+                 type="number"
+                 class="form-control"
+                 step='1'
+                 min='0'
+                 id="pages">
+              </div>
             </div>
-           <div class="col-6">
-             <!-- col 6 -->
+          <div class="col-md-6">
+             <div class="form-group">
+              <label for="wikipidia_link">wikipidia's Link</label>
+              <input name="year"
+                type="number"
+                class="form-control"
+                id="wikipidia_link">
+             </div>
+             <div class="form-group">
+              <label for="language">Language</label>
+              <input name="language"
+                type="text"
+                class="form-control"
+                id="language">
+             </div>
+             <div class="form-group">
+              <label for="published_year">Published year</label>
+              <input name="pub_year"
+                type="number"
+                class="form-control"
+                id="published_year">
+             </div>
+             <div class="form-group">
+              <label for="country">Country</label>
+              <input name="country"
+                type="text"
+                class="form-control"
+                id="country">
+             </div>
            </div>
-       </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <button type="button" class="btn btn-lg bg-danger">Submit</button>
+          </div>
+        </div>
       </form>
     </div>
    </body>
